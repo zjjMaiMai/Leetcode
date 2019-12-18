1b540c385343f73a11b20a76e20c237b5f286ff5
zjjMaiMai <zjjMaiMai@users.noreply.github.com>
2019-12-19 00:57:00 +0800

    Fix 0153 to 0154

--- Leetcode/0154_find-minimum-in-rotated-sorted-array-ii/main.cpp
+++ Leetcode/0154_find-minimum-in-rotated-sorted-array-ii/main.cpp
@@ -5,19 +5,23 @@
 
 请找出其中最小的元素。
 
-你可以假设数组中不存在重复元素。
+注意数组中可能存在重复的元素。
 
-示例 1:
+示例 1：
 
-输入: [3,4,5,1,2]
+输入: [1,3,5]
 输出: 1
-示例 2:
+示例 2：
 
-输入: [4,5,6,7,0,1,2]
+输入: [2,2,2,0,1]
 输出: 0
+说明：
+
+这道题是 寻找旋转排序数组中的最小值 的延伸题目。
+允许重复会影响算法的时间复杂度吗？会如何影响，为什么？
 
 来源：力扣（LeetCode）
-链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array
+链接：https://leetcode-cn.com/problems/find-minimum-in-rotated-sorted-array-ii
 著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
 */
 
@@ -76,5 +80,7 @@ int main(int argv, char* argc[])
     Solution solution;
     assert(solution.findMin(std::vector<int>{3, 4, 5, 1, 2}) == 1);
     assert(solution.findMin(std::vector<int>{4, 5, 6, 7, 0, 1, 2}) == 0);
+    assert(solution.findMin(std::vector<int>{1, 3, 5}) == 1);
+    assert(solution.findMin(std::vector<int>{2, 2, 2, 0, 1}) == 0);
     return 0;
 }
